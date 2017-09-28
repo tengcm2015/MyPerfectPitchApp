8bf4266a74ca44c4b7454ab13fa48b1a880a22a8
trainshogi <densyashogi@gmail.com>
2017-09-28 14:53:22 +0900

    結果UI改善済。

--- app/src/main/java/com/example/train/myperfectpitchapp/ResultActivity.java
+++ app/src/main/java/com/example/train/myperfectpitchapp/ResultActivity.java
@@ -2,9 +2,12 @@ package com.example.train.myperfectpitchapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.TextViewCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.beardedhen.androidbootstrap.AwesomeTextView;
 
 /**
  * Created by train on 2017/07/03.
@@ -22,7 +25,8 @@ public class ResultActivity extends MainActivity implements View.OnClickListener
 
         //レベル文字列取得・配置
         int resid = getResources().getIdentifier("difficulty_" + String.valueOf(result[0]),"string",getPackageName());
-        TextView textView = (TextView)findViewById(R.id.textView_result_1);
+        TextView textView = (TextView) findViewById(R.id.textView_result_1);
+        TextViewCompat.setAutoSizeTextTypeWithDefaults(textView, TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM);
         textView.setText(resid);
 
         //結果配置
@@ -32,8 +36,11 @@ public class ResultActivity extends MainActivity implements View.OnClickListener
         //コメント配置
         String[] comment = getResources().getStringArray(R.array.result_comment);
         int comment_num = (int)(((float)result[2] / (float)result[1]) * (float)(comment.length - 1));
-        textView = (TextView)findViewById(R.id.textView_result_3);
+        textView = (TextView) findViewById(R.id.textView_result_3);
         textView.setText(comment[comment_num]);
+
+        //文字サイズ変更
+
 
         //ボタンonClick用
         Button returnButton1 = (Button)findViewById(R.id.button_result_1);
